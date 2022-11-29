a232531ce69ccce3e27ba0816ff0e8c47615f5d4
kernel_j <jenniferwong89@gmail.com>
2022-11-28 22:59:48 -0500

    add api key generation to user create op

--- app/operations/user/create.rb
+++ app/operations/user/create.rb
@@ -1,21 +1,47 @@
 class User::Create < Transactions::MonadBase
   def call(params:)
-    Ohm.redis = Redic.new(ENV["REDIS_URL"])
-
     yield Authentications::Validator.call(password: params[:password])
 
     hashed = BCrypt::Password.create(params[:password] + ENV['PEPPER'])
 
+    user = yield create_user(params, hashed)
+
+    api_key = yield create_api_key(user)
+
+    Success(api_key)
+
+  end
+
+  def create_user(params, hashed)
     begin
       user = User.create(:username => params[:username].downcase, :password => hashed)
 
       if user.save
-        Success()
+        Success(user)
       else
         Failure(error: ['failed to save user'])
       end
     rescue => error
       Failure(error: ['username already exists'])
+    end
+  end
+
+  def create_api_key(user)
+    begin
+      api_key = ApiKey.create(
+        :bearer_id => user.id,
+        :bearer_type => user.class.name,
+        :token => SecureRandom.hex,
+        :timestamp => DateTime.now
+      )
+
+      if api_key.save
+        Success(api_key)
+      else
+        Failure(error: ["failed to create api_key for user_id: #{user.id}"])
+      end
+    rescue => error
+      Failure(error: ['token already exsits'])
     end
   end
 end
