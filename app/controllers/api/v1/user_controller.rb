580fd1bb35e41b33de3af7a88a99bad75a03411f
kernel_j <jenniferwong89@gmail.com>
2022-11-28 23:00:56 -0500

    add api controller for user

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class UserController < ApplicationController
+      def create
+        result = User::Create.call(params: user_params)
+
+        if result.success?
+          render json: result.success.to_hash, status: :created
+        else
+          render json: result.failure, status: :bad_request
+        end
+      end
+
+      private 
+
+      def user_params
+        params.require(:user).permit(:username, :password)
+      end
+    end
+  end
+end
